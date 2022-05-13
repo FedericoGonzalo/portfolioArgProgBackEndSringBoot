4ad6d28b69a4886011a571c85be85ec6e69f4c14
Federico Suárez Martín <f.suarezmartin@gmail.com>
2022-05-13 16:18:49 -0300

    aaa

--- src/main/java/com/backEndApp/PortfoliobackEnd/controller/AuthController.java
+++ src/main/java/com/backEndApp/PortfoliobackEnd/controller/AuthController.java
@@ -35,9 +35,12 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.HashSet;
 import java.util.Set;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api/auth")
+//@CrossOrigin(origins = "http://localhost:4200")
+
 public class AuthController {
 
     @Autowired
--- src/main/java/com/backEndApp/PortfoliobackEnd/security/WebSecurity.java
+++ src/main/java/com/backEndApp/PortfoliobackEnd/security/WebSecurity.java
@@ -10,15 +10,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 /**
  *
  * @author BASSTARD
@@ -62,14 +66,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+      
+      
+          http.csrf().disable();
+          http.sessionManagement().sessionCreationPolicy(STATELESS);
+          http.anonymous();
+          http.authorizeHttpRequests().anyRequest().permitAll();
+                  
+                
+   
+  
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
     }
+   
+    
 }
