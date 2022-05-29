ea06edbb902976f205e52a182e4c4d6bb710e838
Federico Suárez Martín <f.suarezmartin@gmail.com>
2022-05-29 18:57:21 -0300

    modxup

--- src/main/java/com/backEndApp/PortfoliobackEnd/security/WebSecurity.java
+++ src/main/java/com/backEndApp/PortfoliobackEnd/security/WebSecurity.java
@@ -8,19 +8,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
 /**
  *
  * @author BASSTARD
  */
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -59,14 +61,22 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
       
          
- 
-       http.cors()
-        
-       .and().csrf().disable() ;
-       
-        //  http.sessionManagement().sessionCreationPolicy(STATELESS);
+   http.csrf().disable();
+          http.sessionManagement().sessionCreationPolicy(STATELESS);
           http.anonymous();
           http.authorizeHttpRequests().anyRequest().permitAll();
+                  
+                
+   
+  
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+      // http.cors()
+        
+       //.and().csrf().disable() ;
+       
+        //  http.sessionManagement().sessionCreationPolicy(STATELESS);
+       //   http.anonymous();
+         // http.authorizeHttpRequests().anyRequest().permitAll();
                   
                 
    
