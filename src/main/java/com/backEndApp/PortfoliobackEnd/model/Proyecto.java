e6077d39714c4be1f72230473748d33337b775f0
Federico Suárez Martín <f.suarezmartin@gmail.com>
2022-05-30 13:21:59 -0300

    ultimo fix proyecto cargaba un dia antes

--- src/main/java/com/backEndApp/PortfoliobackEnd/model/Proyecto.java
+++ src/main/java/com/backEndApp/PortfoliobackEnd/model/Proyecto.java
@@ -3,7 +3,7 @@ package com.backEndApp.PortfoliobackEnd.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import java.util.Date;
+import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,8 +11,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,11 +25,11 @@ public class Proyecto {
     private String nombre;
    
    // @Temporal(javax.persistence.TemporalType.DATE)
-  @Temporal(TemporalType.DATE)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy",timezone = "America/Argentina/Buenos_Aires")
+  //@Temporal(TemporalType.DATE)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
   
 
-    private Date fechaRealizacion;
+    private LocalDate fechaRealizacion;
     private String descripcion;
     private String urlProyecto;
     private String urlImagenProyecto;
@@ -45,7 +43,7 @@ public class Proyecto {
     public Proyecto(){};
     public Proyecto(Long idProyecto,
                     String nombre,
-                    Date fechaRealizacion,
+                    LocalDate fechaRealizacion,
                      String descripcion,
                      String urlProyecto,
                      String urlImagenProyecto,
